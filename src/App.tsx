2b8b4d4a10f2809c795454f112553c4f5f08cb2e
MateusPequeno <wonapack@gmail.com>
2021-05-19 22:04:26 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import 'react-native-gesture-handler';
+/* Some terminal commands
+ adb kill-server
+ adb devices
+ yarn start
+yarn run react-native run-android
+yarn add eslint -D
+yarn eslint --init
+yarn add eslint-config-prettier eslint-plugin-prettier -D
+yarn add eslint-import-resolver-typescript -D
+yarn add styled-components
+yarn add @react-navigation/native
+yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+yarn add @react-navigation/stack
+*/
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './routes/index';
+
+const app: React.FC = () => (
+  <NavigationContainer>
+  <StatusBar barStyle = "light-content" translucent />
+  <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+  <Routes/>
+  </View>
+  </NavigationContainer>
+);
+
+export default app;
